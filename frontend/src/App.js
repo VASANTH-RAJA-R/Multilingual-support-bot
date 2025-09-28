68c43106703b6687871a9fed5919569f45214e55
VASANTH-RAJA-R <vasanthraja022005@gmail.com>
2025-09-28 21:19:53 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ChatBox from "./components/ChatBox";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>🌐 Multilingual Support Bot 🤖💬</h1>
+      <ChatBox />
+    </div>
+  );
+}
+
+export default App;
